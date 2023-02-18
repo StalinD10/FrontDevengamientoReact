59f48500e46f9d2baffabbf247b4d6eb1da7c553
david <davidsandovalmh5o@hotmail.com>
2023-02-17 23:07:20 -0500

    V10

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
+import { defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv'
+
+dotenv.config();
+let  port = process.env.VITE_API_PORT;
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: port,
+  }
 })
